1fdc00bbb26397d8c8686f23124d74d5c09f3ddc
刘圣印 <shengyinliu@outlook.com>
2017-10-26 17:33:57 +0800

    blog list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+
 import { MatToolbarModule } from './components/toolbar'
 import { MatButtonModule } from './components/button'
 import { MatIconModule } from './components/icon'
+import { MaterialModule } from './material-module'
+import {ALL_ROUTES} from './routes';
 
 import { AppComponent } from './app.component';
 import { AppDrawerComponent } from './components/app-drawer/app-drawer.component';
 import { BlogComponent } from './pages/blog/blog.component';
-// import { AppToolbarComponent } from './components/app-toolbar/app-toolbar.component';
 
 
 @NgModule({
@@ -18,9 +22,12 @@ import { BlogComponent } from './pages/blog/blog.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MaterialModule,
+    RouterModule.forRoot(ALL_ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,18 +1,81 @@
 import {Component, ElementRef, Renderer2, ViewEncapsulation} from '@angular/core';
 
+
+/**
+ * Home component for welcome message in DemoApp.
+ */
+@Component({
+  selector: 'home',
+  template: `
+    <p>Welcome to the development demos for Angular Material!</p>
+    <p>Open the sidenav to select a demo.</p>
+  `
+})
+export class Home {}
+
+
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  styleUrls: ['./blog.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  preserveWhitespaces: false,
 })
 export class BlogComponent {
   dark = false;
+  items = [
+    {category: '前端',subCategory:['HTML','CSS','Javascript','AngularJS','工程化']},
+    {category: '后端',subCategory:['PHP','SQL','APACHE']},
+    {category: '工具',subCategory:['git','IDE','Docker']},
+    {category: '测试',subCategory:['Jasmine']},
+    {category: '其它',subCategory:['面向对象']},
+  ];
+  navItems = [
+    {name: 'Autocomplete', route: '/autocomplete'},
+    {name: 'Button Toggle', route: '/button-toggle'},
+    {name: 'Button', route: '/button'},
+    {name: 'Card', route: '/card'},
+    {name: 'Checkbox', route: '/checkbox'},
+    {name: 'Chips', route: '/chips'},
+    {name: 'Datepicker', route: '/datepicker'},
+    {name: 'Dialog', route: '/dialog'},
+    {name: 'Drawer', route: '/drawer'},
+    {name: 'Expansion Panel', route: '/expansion'},
+    {name: 'Focus Origin', route: '/focus-origin'},
+    {name: 'Gestures', route: '/gestures'},
+    {name: 'Grid List', route: '/grid-list'},
+    {name: 'Icon', route: '/icon'},
+    {name: 'Input', route: '/input'},
+    {name: 'List', route: '/list'},
+    {name: 'Live Announcer', route: '/live-announcer'},
+    {name: 'Menu', route: '/menu'},
+    {name: 'Overlay', route: '/overlay'},
+    {name: 'Platform', route: '/platform'},
+    {name: 'Portal', route: '/portal'},
+    {name: 'Progress Bar', route: '/progress-bar'},
+    {name: 'Progress Spinner', route: '/progress-spinner'},
+    {name: 'Radio', route: '/radio'},
+    {name: 'Ripple', route: '/ripple'},
+    {name: 'Screen Type', route: '/screen-type'},
+    {name: 'Select', route: '/select'},
+    {name: 'Sidenav', route: '/sidenav'},
+    {name: 'Slide Toggle', route: '/slide-toggle'},
+    {name: 'Slider', route: '/slider'},
+    {name: 'Snack Bar', route: '/snack-bar'},
+    {name: 'Stepper', route: '/stepper'},
+    {name: 'Table', route: '/table'},
+    {name: 'Tabs', route: '/tabs'},
+    {name: 'Toolbar', route: '/toolbar'},
+    {name: 'Tooltip', route: '/tooltip'},
+    {name: 'Typography', route: '/typography'}
+  ];
+
   constructor(
     private _element: ElementRef,
     private _renderer: Renderer2) {}
 
   toggleFullscreen() {
-    let elem = this._element.nativeElement.querySelector('.blog-root');
+    let elem = this._element.nativeElement.querySelector('.demo-root');
     if (elem.requestFullscreen) {
       elem.requestFullscreen();
     } else if (elem.webkitRequestFullScreen) {
